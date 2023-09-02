1d03510658fe4c902e5408564a370d8424c6a9f6
Ammar <ammaralam37@gmail.com>
2023-09-02 18:01:02 -0400

    Add function that compares choices & decides winner
    This function will take the player and computer choices & decide winner

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ function getComputerChoice(){
         case SCISSORS: return "scissors"
     }
 }
+
+// Returns who won the round as a string
+function playRound(playerSelection, computerSelection){
+    playerSelection = String(playerSelection).toLowerCase();
+    // Check who won
+    if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        return "You win! Rock beats scissors.";
+    } else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+        return "You lose! Scissors beats paper."
+    } else if(playerSelection === 'scissors' && computerSelection === 'scissors'){
+        return "Tie!"
+    } else if(playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You lose! Paper beats rock."
+    } else if(playerSelection === 'paper' && computerSelection === 'paper') {
+        return "Tie!"
+    } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You win! Scissors beats paper."
+    } else if(playerSelection === 'rock' && computerSelection === 'rock') {
+        return "Tie!"
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You win! Paper beats rock."
+    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+        return "You lose! Rock beats scissors."
+    }
+}
